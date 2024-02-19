a35e5ee96349e6b247c1c0d6f153c9be46733e43
samiursakib <rafisamiur@gmail.com>
2024-02-19 21:16:33 +0600

    express app constructed

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+
+app.get((req, res) => {
+  res.send({ title: 'get request at root' });
+});
+const server = http.createServer(app);
+
+server.listen(80, () => console.log('server running at localhost:80'));
